7abfa53298ea944b82989aaaeb0a31598a7ab625
jimmykimani <james.kimani@andela.com>
2018-03-14 13:08:44 +0300

    feat(dashboard): add map styling to dashboaard

--- src/components/Profile/GoogleMap.component.jsx
+++ src/components/Profile/GoogleMap.component.jsx
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 import "./GoogleMap.css";
 
 export default class GoogleMap extends Component {
+  constructor(props) {
+    super(props);
+  }
   componentDidMount() {
+    console.log('props\n\n', this.props)
     let doctor_map = new window.google.maps.Map(
       document.getElementById("doctor-geo"),
       {
@@ -27,7 +31,7 @@ export default class GoogleMap extends Component {
 
   render() {
     return (
-      <div id="doctor-geo" className="map-container">
+      <div id="doctor-geo" style={this.props.style}>
         {this.doctor_geo}
       </div>
     );
--- src/pages/doctorProfile/doctorProfile.component.jsx
+++ src/pages/doctorProfile/doctorProfile.component.jsx
@@ -11,6 +11,12 @@ import "./doctorProfile.css";
 
 export default class DoctorProfile extends Component {
   render() {
+    const mapStyle = {
+      width: '100%',
+      height: '200px',
+        marginTop: '2rem',
+       marginBottom: '1.5rem',
+    }
     return (
       <div>
         <NavBar />
@@ -18,7 +24,7 @@ export default class DoctorProfile extends Component {
           <div className="doctor-profile-container">
             <ProfileHeader history={this.props.history} />
             <ProfileBio />
-            <GoogleMap />
+            <GoogleMap style={mapStyle}/>
             <ProfileTab />
           </div>
         </div>
