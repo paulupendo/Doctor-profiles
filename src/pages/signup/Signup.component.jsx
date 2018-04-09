dbf3a087184f1f589a3c2114098859d397ef87ef
paulupendo <lovepaul46@outlook.com>
2018-04-09 19:21:30 +0300

    feat(physician-finder):Additional details UI

--- src/pages/signup/Signup.component.jsx
+++ src/pages/signup/Signup.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Step, Segment, Button, Divider } from 'semantic-ui-react';
+import { Step, Segment, Button, Divider, Input } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 // components
@@ -9,72 +9,136 @@ import { Header } from '../../common/header/Header.component';
 import './Signup.css';
 
 class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: false,
+      active: true,
+      activeForm: 'Required'
+    };
+  }
   componentWillMount() {
-    Object.keys(this.props.doctorDetails).length === 0 &&
-      this.props.history.push('/');
+    // Object.keys(this.props.doctorDetails).length === 0 &&
+    //   this.props.history.push('/');
   }
 
+  handleClick = () => {
+    this.setState({
+      isActive: true,
+      active: false,
+      activeForm: 'Additional'
+    });
+  };
+
+  handleActiveForm = () => {
+    this.setState({
+      isActive: false,
+      active: true,
+      activeForm: 'Required'
+    });
+  };
+
   render() {
+    const img =
+      'http://res.cloudinary.com/dlqcyupez/image/upload/v1523277200/field-profilephoto.svg';
     return (
       <div className="sign-up-container">
         <Header />
         <div className="inner-container">
           <Step.Group>
-            <Step active>
+            <Step active={this.state.active} onClick={this.handleActiveForm}>
               <Step.Content>
                 <Step.Description>Required Details</Step.Description>
               </Step.Content>
             </Step>
-            <Step>
+            <Step active={this.state.isActive}>
               <Step.Content>
                 <Step.Description>Additional Details</Step.Description>
               </Step.Content>
             </Step>
           </Step.Group>
-          <Segment attached>
-            <div className="header-info">
-              <div className="title">BASIC INFORMATION</div>
-              <div>
-                <Button content="EDIT" />
+          {this.state.activeForm === 'Required' ? (
+            <Segment attached>
+              <div className="header-info">
+                <div className="title">BASIC INFORMATION</div>
+                <div>
+                  <Button content="EDIT" />
+                </div>
               </div>
-            </div>
-            <div className="personal-info">
-              <div>
-                <span>FULL NAME</span>
-                <h5>{this.props.doctorDetails.fullNames}</h5>
+              <div className="personal-info">
+                <div>
+                  <span>FULL NAME</span>
+                  <h5>{this.props.doctorDetails.fullNames}</h5>
+                </div>
+                <div>
+                  <span>PHONE NUMBER</span>
+                  <h5>{this.props.doctorDetails.phoneNumber}</h5>
+                </div>
               </div>
-              <div>
-                <span>PHONE NUMBER</span>
-                <h5>{this.props.doctorDetails.phoneNumber}</h5>
+              <div className="doc-specialty">
+                <div>
+                  <span>SPECIALTY</span>
+                  <h5>
+                    {Object.keys(this.props.doctorDetails).length !== 0 && [
+                      ...this.props.doctorDetails.specialties
+                    ]}
+                  </h5>
+                </div>
               </div>
-            </div>
-            <div className="doc-specialty">
-              <div>
-                <span>SPECIALTY</span>
-                <h5>
-                  {Object.keys(this.props.doctorDetails).length !== 0 && [
-                    ...this.props.doctorDetails.specialties,
-                  ]}
-                </h5>
+              <Divider />
+              <div className="medical-license-box">
+                <div>* MEDICAL LICENSE</div>
+                <div>
+                  <h5>
+                    For verification please provide us with a copy of your
+                    medical license
+                  </h5>
+                </div>
+                <div>
+                  <Button content="UPLOAD" />
+                </div>
               </div>
-            </div>
-            <Divider />
-            <div className="medical-license-box">
-              <div>* MEDICAL LICENSE</div>
-              <div>
-                <h5>
-                  For verification please provide us with a copy of your medical
-                  license
-                </h5>
+            </Segment>
+          ) : (
+            <Segment attached>
+              <div className="additional-details">
+                <div className="additional-header">
+                  <img src={img} />
+                  <div className="user-title">
+                    <h2>
+                      {' '}
+                      {this.props.doctorDetails.fullNames || 'Jimmy Kimani'}
+                    </h2>
+                    <span>
+                      {' '}
+                      {this.props.doctorDetails.phoneNumber || '+899812345678'}
+                    </span>
+                    <h5>
+                      {Object.keys(this.props.doctorDetails).length !== 0 && [
+                        ...this.props.doctorDetails.specialties
+                      ]}
+                    </h5>
+                  </div>
+                  <div className="additinal-info">
+                    <Input placeholder="Email-address" />
+                    <Input placeholder="City/State" />
+                  </div>
+                </div>
+                <Divider />
+                <div className="education-box">
+                  <div>EDUCATION</div>
+                  <div className="input-container">
+                    <Input placeholder="School or Training Institution" />
+                    <Input placeholder="Year-Graduated " />
+                  </div>
+                  <Input placeholder="Qualification" />
+                </div>
               </div>
-              <div>
-                <Button content="UPLOAD" />
-              </div>
-            </div>
-          </Segment>
+            </Segment>
+          )}
         </div>
         <div className="btn">
-          <Button content="NEXT" />
+          <Button content="NEXT" onClick={this.handleClick} />
         </div>
       </div>
     );
@@ -82,7 +146,7 @@ class SignUp extends Component {
 }
 
 const mapStateToProps = state => ({
-  doctorDetails: state.signUpStats.doctorDetails,
+  doctorDetails: state.signUpStats.doctorDetails
 });
 
 export default connect(mapStateToProps)(SignUp);
